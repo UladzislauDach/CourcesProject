9bf9574e38a34984c507b3b2634ea8c408ed5a77
Uladzislau Dach <vladzayac1@gmail.com>
2021-08-16 23:09:55 +0300

    Edit url path

--- src/main/java/lesson8/homework/controller/web/filters/SecurityFilter.java
+++ src/main/java/lesson8/homework/controller/web/filters/SecurityFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns ={"/messenger/chats","/messenger/message"})
+@WebFilter(urlPatterns = {"/messenger/chats", "/messenger/message"})
 public class SecurityFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -18,9 +18,10 @@ public class SecurityFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String contextPath = req.getContextPath();
+
         HttpSession session = req.getSession();
-        if (session!=null && session.getAttribute("login")!= null){
-            chain.doFilter(request,response);
+        if (session != null && session.getAttribute("login") != null) {
+            chain.doFilter(request, response);
         } else
             res.sendRedirect(contextPath + "/messenger/signIn");
     }
